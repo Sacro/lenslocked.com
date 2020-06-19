2254cb9bba4c256867f2c2266ace63dff4407bab
Ben Woodward <ben@benwoodward.me.uk>
2020-06-19 21:26:37 +0100

    refactor: cleaning up view.go

--- views/view.go
+++ views/view.go
@@ -2,14 +2,16 @@ package views
 
 import (
 	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir         string = "views/layouts"
+	TemplateExtension string = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -25,4 +27,14 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFiles returns a slice of strings representing
+// the layout files used in our application
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExtension)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
