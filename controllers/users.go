f24962bcbcc62d47bf71df3b8bda14885a1d0435
Ben Woodward <ben@benwoodward.me.uk>
2020-06-19 22:25:02 +0100

    wip: first draft of parsing signup form

--- controllers/users.go
+++ controllers/users.go
@@ -35,6 +35,11 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 // submits it. This is used to create a new user account.
 //
 // POST /signup
-func (u *Users) Create(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintln(w, "This is a temporary response.")
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
