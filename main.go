b1af1b730d2d4e75a9a71ee7aa3d9472e4f8fd38
Ben Woodward <ben@benwoodward.me.uk>
2020-06-19 11:15:11 +0100

    feat: Added a contact page

--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, _ *http.Request) {
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch, please send an email to <ahref=\"mailto:support@lenslocked.com\">support@lenslocked.com</ahref=>.")
+	}
+
 }
 
 func main() {
