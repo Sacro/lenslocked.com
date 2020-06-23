e609eaec86c8f989249ec63ff6b1b5838b3cad4a
Ben Woodward <ben@benwoodward.me.uk>
2020-06-23 17:21:21 +0100

    feat: adding the user service

--- models/users.go
+++ models/users.go
@@ -1,9 +1,51 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		panic(err)
+	}
+	return &UserService{db}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// ByID will look up by the id provided.
+// 1 - user, nil
+// 2 - nil, ErrNotFound
+// 3 - nil, otherError
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+
+	default:
+		return nil, err
+	}
+}
+
+// Close the userservice database connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
 
 type User struct {
 	gorm.Model
