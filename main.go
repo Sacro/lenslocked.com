2b6cd0f1de041082ba36e25aa61c70457a53cf3a
Ben Woodward <ben@benwoodward.me.uk>
2020-06-28 17:26:07 +0100

    feat: created the login form

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ func main() {
 	r.Handle("/contact", staticC.ContactView).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	r.Handle("/login", usersC.LoginView).Methods("GET")
+	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
